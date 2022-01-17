2826499aa0d9a6a65b051329df66fa90c88129c0
pylypchukroman <romanpylypchuk@gmail.com>
2022-01-17 23:40:26 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-import './sass/main.scss';
+import './css/style.css'
+import { fetchImages } from './js/fetch-images'
+import { renderGallery } from './js/render-gallery'
+import Notiflix from 'notiflix'
+import SimpleLightbox from 'simplelightbox'
+import 'simplelightbox/dist/simple-lightbox.min.css'
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.btn-load-more');
+let simpleLightBox;
+
+searchForm.addEventListener('submit', onSearchForm)
+moreBtn.addEventListener('click', loadMoreBtn)
+
+let query = '';
+let page = 1;
+const perPage = 40;
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1
+  query = e.currentTarget.searchQuery.value.trim()
+  gallery.innerHTML = ''
+  moreBtn.classList.add('is-hidden')
+
+  if (query === '') {
+     Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
+    return
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+      } else {
+        renderGallery(data.hits)
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+
+        if (data.totalHits > perPage) {
+          moreBtn.classList.remove('is-hidden')
+        }
+      }
+    })
+    .catch(error => console.log(error))
+}
+
+function loadMoreBtn() {
+  page += 1
+  simpleLightBox.destroy()
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits)
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+
+      const lastPages = Math.ceil(data.totalHits / perPage)
+
+      if (page > lastPages) {
+        moreBtn.classList.add('is-hidden')
+         Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+      }
+    })
+    .catch(error => console.log(error))
+}
---
+++ src/js/render-gallery.js
@@ -0,0 +1,27 @@
+export { renderGallery }
+
+const gallery = document.querySelector('.gallery')
+// const galleryEl = document.querySelector('.gallery .a')
+
+function renderGallery(images) {
+  const markup = images
+      .map(image => {
+        // console.log(image);
+      return `
+        <a class="gallery-link" href="${image.largeImageURL}">
+          <div class="photo-card">
+            <img class="photo-card-img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${image.likes}</p>
+              <p class="info-item"><b>Views</b>${image.views}</p>
+              <p class="info-item"><b>Comments</b>${image.comments}</p>
+              <p class="info-item"><b>Downloads</b>${image.downloads}</p>
+            </div>
+          </div>
+        </a>
+      `
+    })
+    .join('')
+
+  gallery.insertAdjacentHTML('beforeend', markup)
+}
